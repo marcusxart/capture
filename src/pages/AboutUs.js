10f40b0580a8839e7f4c21cf5b93c5fd1cc50505
marcusxart <cemeji64@gmail.com>
2022-05-15 11:33:58 +0100

    updating about js

--- src/pages/AboutUs.js
+++ src/pages/AboutUs.js
@@ -3,6 +3,7 @@ import React from "react";
 import AboutSection from "../components/AboutSection";
 import ServicesSection from "../components/ServicesSection";
 import FaqSection from "../components/FaqSection";
+import styled from "styled-components";
 //Animations
 import { motion } from "framer-motion";
 import { pageAnimation } from "../animation";
