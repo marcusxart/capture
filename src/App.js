b37b866fae8cf8f6ac120d04f8476e790ad4b848
marcusxart <cemeji64@gmail.com>
2022-05-15 11:05:37 +0100

    finshed

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from "react";
+//Global Style
+import GlobalStyle from "./components/GlobalStyle";
+//Import Pages
+import AboutUs from "./pages/AboutUs";
+import ContactUs from "./pages/ContactUs";
+import OurWork from "./pages/OurWork";
+import Nav from "./components/Nav";
+import MovieDetail from "./pages/MovieDetail";
+//Router
+import { Routes, Route, useLocation } from "react-router-dom";
+//Animation
+import { AnimatePresence } from "framer-motion";
+
+function App() {
+  const location = useLocation();
+
+  return (
+    <div className="App">
+      <GlobalStyle />
+      <Nav />
+      <AnimatePresence exitBeforeEnter>
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<AboutUs />} />
+          <Route path="/work" element={<OurWork />} />
+          <Route path="/work/:id" element={<MovieDetail />} />
+          <Route path="/contact" element={<ContactUs />} />
+        </Routes>
+      </AnimatePresence>
+    </div>
+  );
+}
+
+export default App;
